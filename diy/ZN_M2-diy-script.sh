c55a5fe885baa8a122c6a1251b9cf39f6b480f71
xysck <xysck@126.com>
2025-11-24 10:30:52 +0800

    Update ZN_M2-diy-script.sh

--- diy/ZN_M2-diy-script.sh
+++ diy/ZN_M2-diy-script.sh
@@ -51,6 +51,9 @@ function git_sparse_clone() {
 
 # Themes
 
+# 更改 Argon 主题背景
+cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+
 # SmartDNS
 # git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
 # git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
